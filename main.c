7dfe049439c63cfad1361e092a28db7ac5bf1a85
Joshua Bays <jb0401@uah.edu>
2023-12-13 16:29:54 -0600

    created rudimentary command system, close audio device

--- main.c
+++ main.c
@@ -33,6 +33,9 @@
 enum Modes{ currenttime, timer };
 
 int main(int argc, char *argv[]){
+	unsigned st = time(0);
+	int offset = 0;
+	
 	SDL_Renderer *renderer;
 	SDL_Window *window;
 	
@@ -55,36 +58,61 @@ int main(int argc, char *argv[]){
 	SDL_AudioSpec wavSpec;
 	Uint32 wavLength;
 	Uint8 *wavBuffer;
+	SDL_AudioDeviceID deviceId;
 	
 	_Bool hours = 1;
 	_Bool secs = 1;
-	char mode = currenttime;
-	//char mode = timer;
-	
-	time_t now;
-	struct tm *tm_struct;
-	char dispStr[16];
-	unsigned hms[3];
-	
-	time_t initTime = time(0);
-	unsigned timerLen = 5;
-	long int remtime;
 	_Bool timerAlarm = 0;
+	_Bool alarmClosed = 1;
+	char mode = currenttime;
+	long int remtime;
+	unsigned hms[3];
+	unsigned alarmLen = 5;
+	unsigned timerLen = 5;
 	unsigned timerChangeTime = 60;
+	char dispStr[16];
+	time_t now;
+	time_t initTime = time(0);
+	time_t alarmTime;
+	struct tm *tm_struct;
+	
+	FILE* readfile = fopen("cmd.txt", "r");
+	char buff[32];
+	fgets(buff, sizeof(buff), readfile);
 	
 	while(1){
+		if(strlen(buff) > 0){
+			if(strncmp(buff, "set timer ", 10) == 0){
+				mode = timer;
+				timerLen = (buff[10] - '0') * 36000;
+				timerLen += (buff[11] - '0') * 3600;
+				timerLen += (buff[13] - '0') * 600;
+				timerLen += (buff[14] - '0') * 60;
+				timerLen += (buff[16] - '0') * 10;
+				timerLen += (buff[17] - '0');
+			}else if(strncmp(buff, "set clock", 9) == 0){
+				mode = currenttime;
+			}else if(strncmp(buff, "toggle seconds", 14) == 0){
+				secs = !secs;
+			}else if(strncmp(buff, "set now ", 8) == 0){
+				sscanf(buff + 8, "%x", &offset);
+				offset = offset - st;
+			}
+			for(unsigned i = 0; i < sizeof(buff); i++){ buff[i] = '\0'; }
+		}
 		if(mode == currenttime){
-			now = time(NULL); tm_struct = localtime(&now);
+			now = time(NULL) + offset; tm_struct = localtime(&now);
 			hms[0] = tm_struct->tm_hour; hms[1] = tm_struct->tm_min; hms[2] = tm_struct->tm_sec;
-			
+			if(hms[0] == 0){ hms[0] = 12; }
 		}else if(mode == timer){
 			remtime = timerLen - (time(0) - initTime);
 			if(remtime > 0){
 				hms[0] = floor((remtime) / 3600);
 				hms[1] = floor((remtime - hms[0] * 3600) / 60);
 				hms[2] = remtime % 60;
-				if(remtime <= 60){ hours = 0; secs = 1; }
+				//if(remtime <= 60){ hours = 0; secs = 1; }
 				if(remtime <= timerChangeTime){
+					hours = 0; secs = 1;
 					dispColor.r = AlertTextR;
 					dispColor.g = AlertTextG;
 					dispColor.b = AlertTextB;
@@ -95,9 +123,10 @@ int main(int argc, char *argv[]){
 				if(!timerAlarm){
 					timerAlarm = 1;
 					SDL_LoadWAV(AlarmFilename, &wavSpec, &wavBuffer, &wavLength);
-					SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+					deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
 					SDL_QueueAudio(deviceId, wavBuffer, wavLength);
 					SDL_PauseAudioDevice(deviceId, 0);
+					alarmTime = time(0); alarmClosed = 0;
 					dispColor.r = 255; dispColor.g = 255; dispColor.b = 255;
 					//dispColor.r = 0; dispColor.g = 0; dispColor.b = 0;
 				}
@@ -126,7 +155,7 @@ int main(int argc, char *argv[]){
 						case SDL_SCANCODE_M:
 							if(mode == currenttime){
 								mode = timer;
-								timerLen = 62; initTime = time(0);
+								timerLen = 5; initTime = time(0);
 							}else{
 								mode = currenttime;
 								hours = 1;
@@ -147,6 +176,10 @@ int main(int argc, char *argv[]){
 					}
 					break;
 			}
+		}
+		if(!alarmClosed && time(0) - alarmTime >= alarmLen){
+			SDL_CloseAudioDevice(deviceId);
+			alarmClosed = 1;
 		}
 	
 		dispSurface = TTF_RenderText_Solid(dispFont, dispStr, dispColor);
