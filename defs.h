76602854d7590f46ab58dcef2e0ef0f18ed13533
Joshua Bays <jb0401@uah.edu>
2023-12-17 14:40:39 -0600

    added defs file

---
+++ defs.h
@@ -0,0 +1,27 @@
+#define WinWidth 1
+#define WinHeight 1
+#define WinName "Charger Time"
+
+//#define FontFileName "fonts/Jost/static/Jost-Medium.ttf"
+//#define FontFileName "fonts/Jost/static/Jost-Black.ttf"
+//#define FontFileName "fonts/Silkscreen/Silkscreen-Regular.ttf"
+//#define FontFileName "fonts/Electrolize/Electrolize-Regular.ttf"
+#define FontFileName "/home/josh/School/ETLC/countdown-display/fonts/Nunito/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf"
+
+//#define AlarmFilename "sounds/RR.wav"
+#define AlarmFilename "/home/josh/School/ETLC/countdown-display/sounds/chime.wav"
+
+//#define RendererFlags SDL_RENDERER_ACCELERATED
+#define RendererFlags SDL_RENDERER_PRESENTVSYNC
+//#define WindowFlags SDL_WINDOW_BORDERLESS
+#define WindowFlags SDL_WINDOW_FULLSCREEN_DESKTOP
+
+#define ClockSize 600 // Font size of display
+#define TimerSize 750 // Font size of display
+
+#define DefaultTextR 0
+#define DefaultTextG 119
+#define DefaultTextB 200
+#define AlertTextR 255
+#define AlertTextG 0
+#define AlertTextB 0
